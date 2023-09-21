c2bd5fe3a1dc24d6e071f67d0012b5cb02672e5b
Oki <oki@fungsitama.com>
2023-09-21 09:25:14 +0700

    initial commit

---
+++ src/api/payment-callback/controllers/payment-callback.js
@@ -0,0 +1,42 @@
+'use strict';
+
+/**
+ * payment-callback controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::payment-callback.payment-callback', ({ strapi }) => ({
+  async create(ctx) {
+    let requestData = ctx.request.body;
+    console.log('request: ', requestData);
+
+    let test = await strapi.controller('api::order.order');
+    console.log('test', test);
+
+    let order = await strapi.service('api::order.order').findOne(requestData.data.history.order_id);
+    console.log('order', order);
+
+    return { 'data': order }
+
+
+    // let order = await strapi.service('api::order.order').findOne(requestData.data);
+    // let inputData = { 'data': { 'history': requestData } };
+
+    // const result = await strapi.service('api::payment-callback.payment-callback').create(inputData);
+
+    // let params = {}
+
+    // if (requestData.transaction_status == 'settlement') {
+    //   params = { 'data': { 'statusOrder': 'purchased' } }
+    // } else {
+    //   params = { 'data': { 'statusOrder': 'cancel' } }
+    // }
+
+    // let updateOrder = await strapi.service('api::order.order').update(requestData.order_id, params);
+
+    // console.log('update data: ', updateOrder);
+
+    // return { 'data': updateOrder }
+  }
+}));
